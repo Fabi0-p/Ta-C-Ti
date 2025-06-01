3921be7ca275f495b3577284bbe993cab146c777
Fabio Panasiuk <fabiopanasiuk@gmail.com>
2025-05-31 23:37:52 -0300

    Agregado placeholder jugable para el juego.
    Agregada IA para competir contra el jugador.
    Pendiente:
    - Manejo de errores.
    - Mejoras de visualización
    - Lógica de turnos, múltiples jugadores.
    - Integración con el resto del sistema.
    - Limpieza de código.

--- Juego.h
+++ Juego.h
@@ -1,28 +1,16 @@
 #ifndef JUEGO_H
 #define JUEGO_H
 
-#define EST_EN_CURSO 0
-#define EST_EMPATE 1
-#define EST_GANADO 2
-
-
-enum t_jugador{
-    J_X = -1,
-    J_VACIO = 0,
-    J_O = 1
-};
-
-typedef enum t_jugador t_tablero[9]; 
-typedef short int t_linea[3]; 
+#include "main.h"
 
 void initTablaLineas(t_linea tab[8]);
 void limpiarTablero(t_tablero tab);
 int evalLinea(t_linea linea, t_tablero tablero);
 int evalTablero(t_linea tabLinea[8], t_tablero tablero, enum t_jugador *ganador);
+int evalTableroConGuias(t_linea tabLinea[8], t_tablero tablero, enum t_jugador *ganador);
 int jugarFicha(t_tablero tab, enum t_jugador jug, short int casilla);
 void turnoJugador(t_tablero tab, enum t_jugador jug);
 int tableroCompararEstados(int evalActual, int evalCandidato, enum t_jugador jug);
 void mostrarTablero(t_tablero tab);
-void procesarPartida();
 
 #endif
--- RivalIA.h
+++ RivalIA.h
@@ -2,7 +2,6 @@
 #define RIVAL_IA_H
 
 #include"main.h"
-#include"Juego.h"
 
 int IAbuscarJugada(t_linea tabLinea[8], t_tablero tablero, enum t_jugador jug, int *lineaAJugar);
 void jugarPrimerOpLinea(t_linea linea, t_tablero tab, enum t_jugador jug);
